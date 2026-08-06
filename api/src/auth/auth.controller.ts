3952f5404ee77e204de67acccaa8dc3404ef4c7b
Mark Allan <interioxide@gmail.com>
2026-08-06 18:32:11 +0800

    cookie secure changed

--- api/src/auth/auth.controller.ts
+++ api/src/auth/auth.controller.ts
@@ -15,7 +15,7 @@ export class AuthController {
     ) {
         this.accessTokenCookieOptions = {
             httpOnly: true,
-            secure: true,
+            secure: false,
             sameSite: 'none',
             path: '/',
             domain: configService.get<string>('responseCookie.domain'),
